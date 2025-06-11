474052f6416d143c97d19fa19d59972ec9229ad2
Jesus Alejandro Estrada Gonzalez <estrada.gonzalez.jesusalejandro@gmail.com>
2025-06-10 21:48:54 -0600

    creacion de variable estatica y metodo de impresion

---
+++ src/EjemploClase.java
@@ -0,0 +1,9 @@
+public class EjemploClase {
+    static String coachName = "Eduardo";
+
+    public static void showYourName() {
+        //codifo que muestra el nombre del coah en consola
+        coachName = coachName.toUpperCase();
+        System.out.println("Hola mi nombre es: " + coachName.toLowerCase());
+    }
+}
